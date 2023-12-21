caf23e1c483d5ae4143c82683bd260780fb1d996
Unish Chamling Rai <kirantunish@gmail.com>
2023-12-21 20:20:33 +0545

    repair

--- src/components/CommonHead.jsx
+++ src/components/CommonHead.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const CommonHead = ({subhead}) => {
+const CommonHead = ({ subhead }) => {
   return (
-    <div ClassName='text-white'>
-    
-        <h1 className='text-5xl font-semibold'>{subhead}</h1>
+    <div className="text-white">
+      <h1 className="text-5xl font-semibold">{subhead}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default CommonHead
+export default CommonHead;
